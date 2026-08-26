d261d0c8d498724223bf1f420edd8492bb373c0d
Jonny José Rojas Aguinda <jjrojas@gadpnapo.local>
2026-08-25 20:46:13 -0500

    feat: documentar interfaz Notificador

--- src/main/java/ec/edu/uees/tutorias/service/Notificador.java
+++ src/main/java/ec/edu/uees/tutorias/service/Notificador.java
@@ -1,5 +1,5 @@
 package ec.edu.uees.tutorias.service;
-
+// Interfaz utilizada para desacoplar el servicio de la implementación de notificaciones.
 public interface Notificador {
 
     void enviar(String mensaje);
